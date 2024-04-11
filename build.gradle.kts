08004571f373173c3924005bf3f46df11ad9dadf
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-04-11 23:57:59 +0900

    Add new dependencies and java-library plugin
    The java-library plugin has been added to the build.gradle.kts file. Several dependencies such as "org.mybatis.generator:mybatis-generator-core", "org.mockito:mockito-core", and "org.projectlombok:lombok" have also been added to extend the project's functionalities and to ensure efficient and effective testing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("java-library")
 }
 
 group = "org.venus-pj"
@@ -10,13 +11,18 @@ java {
 repositories {
     mavenCentral()
 }
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.3")
     implementation("com.google.guava:guava:33.1.0-jre")
-
+// https://mvnrepository.com/artifact/org.mybatis.generator/mybatis-generator-core
+    implementation("org.mybatis.generator:mybatis-generator-core:1.4.2")
+// https://mvnrepository.com/artifact/org.mockito/mockito-core
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+// https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.test {
