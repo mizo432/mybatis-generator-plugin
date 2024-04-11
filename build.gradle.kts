0c9604e7658f7af3c1ec4b8803317b3b1d8db2e1
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-04-11 23:33:16 +0900

    Update Java compatibility and add Guava dependency
    The build.gradle.kts file has been updated with two key changes. First, the source compatibility has been set to Java version 21 to ensure the project works with this Java version. Second, a new dependency to the Guava library (version 33.1.0-jre) has been added, potentially to utilize some of its features.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
 
 group = "org.venus-pj"
 version = "1.0-SNAPSHOT"
-
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
 repositories {
     mavenCentral()
 }
@@ -13,6 +15,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.3")
+    implementation("com.google.guava:guava:33.1.0-jre")
+
 }
 
 tasks.test {
